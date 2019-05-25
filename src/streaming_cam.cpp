271ebb7c49e7b43f8ec1ad6c9ded1ac0f3eb8488
micheleantonazzi <micheleantonazzi@gmail.com>
2019-05-25 23:09:11 +0200

    updated master from develop

--- src/streaming_cam.cpp
+++ src/streaming_cam.cpp
@@ -12,6 +12,9 @@
 #include <thread>
 
 using namespace ros;
+using std::string;
+
+
 
 // This struct store the context used by pipeline
 typedef struct {
@@ -22,11 +25,32 @@ typedef struct {
     // If the pipeline is full ? true : false
     bool full;
     GstElement* appSrc;
+
+    // Parameters
+
+    // The topic from which to take frames
+    string topic;
+
+    // Color format
+    string color;
+    int framerate;
+
+    // high-quality  medium-quality  low-quality
+    string profile;
+    // Subpixel motion estimation and partition decision quality: (fast - worst) 1 <= x <= 10 (slow - best)
+    int subme;
+    // Constant quantizer or quality to apply. (high quality) 1 <= x <= 50 (low quality)
+    int quantizer;
+    // Number of threads used by the encoder (0 = auto)
+    int threads;
 } Context;
 
 // FUNCTIONS PROTOTYPES
 
-// This function push the frame inside the pipeline
+// This function stores the parameters
+void store_parameters(Context*);
+
+// This function pushes the frame inside the pipeline
 void push_frame(const sensor_msgs::Image::ConstPtr&, const boost::shared_ptr<Context>&);
 
 // This function is called when the first client connects
@@ -47,19 +71,18 @@ static void too_much_data(GstAppSrc*, Context*);
 // This function is call in a separate thread and it starts the server
 static void start_server(GMainLoop*);
 
-
 int main(int argc, char **argv){
+
+    // Init GStreamer
+    gst_init(NULL, NULL);
 
     // Init ROS
     ros::init(argc, argv, "streaming_cam");
 
-    // Set up Context
     Context* context = new Context();
-    context->width = 640;
-    context->height = 480;
 
-    // Init GStreamer
-    gst_init(NULL, NULL);
+    // Acquire parameters
+    store_parameters(context);
 
     // Build server RTSP
     GMainLoop *loop = g_main_loop_new(NULL, FALSE);
@@ -70,7 +93,7 @@ int main(int argc, char **argv){
     // Set up the pipeline
     gst_rtsp_media_factory_set_launch(factory,
             "( appsrc name=appsrc ! videoconvert ! video/x-raw,format=I420 ! "
-            "x264enc name=encoder ! rtph264pay name=pay0 pt=96 )");
+            "x264enc name=encoder pass=qual ! rtph264pay name=pay0 pt=96 )");
 
     g_signal_connect(factory, "media-configure", (GCallback)media_configure, context);
     // A single factory (pipeline) shared by all clients
@@ -86,12 +109,61 @@ int main(int argc, char **argv){
     ptrContext.reset(context);
 
     NodeHandle node;
-    Subscriber subscriber = node.subscribe<sensor_msgs::Image>("usb_cam/image_raw", 1000,
+    Subscriber subscriber = node.subscribe<sensor_msgs::Image>(context->topic, 1000,
             boost::bind(push_frame, _1, ptrContext));
 
     std::thread startServer(start_server, loop);
 
     ros::spin();
+}
+
+void store_parameters(Context* parameters){
+    ros::NodeHandle nodeHandle("~");
+
+    // Camera paramters
+    if(!nodeHandle.getParam("topic", parameters->topic))
+        ROS_ERROR("Parameter topic missing");
+    nodeHandle.param<string>("color", parameters->color, "RGB");
+    if(!nodeHandle.getParam("width", parameters->width))
+        ROS_ERROR("Parameter width missing");
+    if(!nodeHandle.getParam("height", parameters->height))
+        ROS_ERROR("Parameter height missing");
+
+    // Pipeline parameters
+    nodeHandle.param<int>("framerate", parameters->framerate, 30);
+
+    // Encoder parameters
+    if(!nodeHandle.getParam("profile", parameters->profile))
+        ROS_ERROR("Parameter profile missing");
+
+    if(parameters->profile == "high-quality"){
+        parameters->subme = 8;
+        parameters->quantizer = 10;
+        parameters->threads = 0;
+    }
+    else if(parameters->profile == "medium-quality"){
+        parameters->subme = 5;
+        parameters->quantizer = 21;
+        parameters->threads = 0;
+    }
+    else if (parameters->profile == "low-quality"){
+        parameters->subme = 8;
+        parameters->quantizer = 30;
+        parameters->threads = 0;
+    }
+    else if (parameters->profile == "customized"){
+        if(!nodeHandle.getParam("subme", parameters->subme))
+            ROS_ERROR("Parameter subme missing");
+
+        if(!nodeHandle.getParam("quantizer", parameters->quantizer))
+            ROS_ERROR("Parameter quantizer missing");
+
+        if(!nodeHandle.getParam("threads", parameters->threads))
+            ROS_ERROR("Parameter threads missing");
+    }
+    else
+        ROS_ERROR("Parameter profile wrong");
+
 }
 
 void push_frame(const sensor_msgs::Image::ConstPtr& image, const boost::shared_ptr<Context>& context){
@@ -109,7 +181,7 @@ void push_frame(const sensor_msgs::Image::ConstPtr& image, const boost::shared_p
         memmove(map.data, image->data.data(), gst_buffer_get_size(buffer));
 
         GST_BUFFER_PTS(buffer) = context->timestamp;
-        GST_BUFFER_DURATION(buffer) = gst_util_uint64_scale_int(1, GST_SECOND, 30);
+        GST_BUFFER_DURATION(buffer) = gst_util_uint64_scale_int(1, GST_SECOND, context->framerate);
         context->timestamp += GST_BUFFER_DURATION(buffer);
 
         g_signal_emit_by_name(context->appSrc, "push-buffer", buffer, &ret);
@@ -121,36 +193,53 @@ void push_frame(const sensor_msgs::Image::ConstPtr& image, const boost::shared_p
 
 static void media_configure(GstRTSPMediaFactory*, GstRTSPMedia* media, Context* context) {
 
-    ROS_INFO_STREAM("First client connected, starting pipeline");
+    ROS_INFO("First client connected, starting pipeline");
 
-    context->timestamp = 0;
+    // Connect callback to media signals
+    g_signal_connect(media, "new-state", (GCallback)change_state, context);
+    g_signal_connect(media, "unprepared", (GCallback)unprepared, context);
 
+    // Get appSrc element
     GstElement* element = gst_rtsp_media_get_element(media);
     GstElement* appSrc = gst_bin_get_by_name_recurse_up(GST_BIN(element), "appsrc");
 
+    // Set appSrc parameters
     gst_util_set_object_arg(G_OBJECT(appSrc), "format", "time");
 
+    const string c = context->color;
+
+    // Set appSrc caps
     g_object_set(G_OBJECT(appSrc), "caps",
                 gst_caps_new_simple("video/x-raw",
-                                    "format", G_TYPE_STRING, "RGB",
+                                    "format", G_TYPE_STRING, context->color.c_str(),
                                     "width", G_TYPE_INT, context->width,
                                     "height", G_TYPE_INT, context->height,
-                                    "framerate", GST_TYPE_FRACTION, 30, 1, NULL),
+                                    "framerate", GST_TYPE_FRACTION, context->framerate, 1, NULL),
                 NULL);
 
-    g_signal_connect(media, "new-state", (GCallback)change_state, context);
 
-    g_signal_connect(media, "unprepared", (GCallback)unprepared, context);
-
-    context->clientsConnected = true;
-
-    gst_object_unref(appSrc);
-    gst_object_unref(element);
-
-    context->appSrc = appSrc;
-
+    // Connect appSrc signals to callbacks
     g_signal_connect(appSrc, "need-data", (GCallback)need_data, context);
     g_signal_connect(appSrc, "enough-data", (GCallback)too_much_data, context);
+
+    // Update context
+    context->timestamp = 0;
+    context->clientsConnected = true;
+    context->appSrc = appSrc;
+
+    gst_object_unref(appSrc);
+
+    // Get h264 encoder
+    GstElement* encoder = gst_bin_get_by_name_recurse_up(GST_BIN(element), "encoder");
+
+    // Set encoder parameters
+    g_object_set(G_OBJECT(encoder), "subme", context->subme, NULL);
+    g_object_set(G_OBJECT(encoder), "quantizer", context->quantizer, NULL);
+    g_object_set(G_OBJECT(encoder), "threads", context->threads, NULL);
+
+    gst_object_unref(encoder);
+
+    gst_object_unref(element);
 }
 
 static void change_state(GstRTSPMedia*, gint state, Context* context) {
